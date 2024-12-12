e7be1ab0a9aba6a69075208af03dc092873b91e3
Jermaine Junior <obietrice734@gmail.com>
2024-12-12 19:59:26 +0200

    added auth middleware test endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors'
 import router from './routes/auth';
+import { authMiddleware } from './middlewares/authMiddleware';
 
 const app = express()
 app.use(cors())
@@ -10,5 +11,10 @@ app.use(bodyParser.json())
 const port = process.env.PORT || 3000
 
 app.use('/api/auth', router)
+
+app.get('/test', authMiddleware, (req, res) => {
+  const id = (req as any).userId;
+  res.status(200).json({ message: `access for user with id ${id} is granted` })
+})
 
 app.listen(port, () => console.log(`app is running on port ${port}`))
